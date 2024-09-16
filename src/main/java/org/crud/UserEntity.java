76899013554ac62b96434f524988ac0132c252c5
Muhammad Utoro <dioutoro@googlemail.com>
2024-09-16 12:44:06 +0200

    Connecting and using mongoDB cloud

---
+++ src/main/java/org/crud/UserEntity.java
@@ -0,0 +1,45 @@
+package org.crud;
+
+import io.quarkus.mongodb.panache.PanacheMongoEntity;
+import io.quarkus.mongodb.panache.common.MongoEntity;
+
+@MongoEntity
+public class UserEntity extends PanacheMongoEntity {
+    private String name;
+    private String email;
+    private int age;
+
+    public UserEntity(UserDTO userDTO) {
+        this.name = userDTO.getName();
+        this.email = userDTO.getEmail();
+        this.age = userDTO.getAge();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public UserEntity() {
+
+    }
+}
--- src/main/java/org/crud/UserService.java
+++ src/main/java/org/crud/UserService.java
@@ -1,30 +1,29 @@
 package org.crud;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+//import java.io.File;
+//import java.io.IOException;
+//import java.nio.file.Path;
+//import java.nio.file.Paths;
+import java.util.*;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.fasterxml.jackson.core.type.TypeReference;
+//import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.core.Response;
+//import org.w3c.dom.stylesheets.LinkStyle;
 
 @ApplicationScoped
 public class UserService {
-    private Map<String, UserDTO> users = new HashMap<>();
 
-    // Need to find a way to dynamically adjust the path
-    private static final Path FILE_PATH = Paths.get(System.getProperty("user.dir"), "src", "main", "java", "org", "crud", "users.json");
-
-    public UserService() {
-        loadUsersFromFile();  // Load users from file when starting the service
-    }
+//    private Map<String, UserDTO> users = new HashMap<>();
+//
+//    //Need to find a way to dynamically adjust the path
+//    private static final Path FILE_PATH = Paths.get(System.getProperty("user.dir"), "src", "main", "java", "org", "crud", "users.json");
+//
+//    public UserService() {
+//        loadUsersFromFile();  // Load users from file when starting the service
+//    }
 
     public Response greeting() {
         return Response.ok("Hi swimmers!").build();
@@ -38,8 +37,9 @@ public class UserService {
 
     // Get all users from users map
     public Response getUsers() {
-        Collection<UserDTO> allUsers = users.values();
-        System.out.println("Returning " + allUsers.size() + " users.");  // Debugging line
+//        Collection<UserDTO> allUsers = users.values();
+        List<UserEntity> allUsers = UserEntity.listAll();
+//        System.out.println("Returning " + allUsers.size() + " users.");  // Debugging line
         if (allUsers.isEmpty()) {
             return Response.status(Response.Status.NOT_FOUND).entity("No users are found! Please add one!").build();
         }
@@ -61,66 +61,83 @@ public class UserService {
             return Response.status(Response.Status.BAD_REQUEST).entity("Age must be a positive integer").build();
         }
 
-        String id = UUID.randomUUID().toString();
-        userDTO.setId(id);
-        users.put(id, userDTO);
-        saveUsersToFile();
+        // Converting DTO to entity
+        UserEntity userEntity = new UserEntity();
+        userEntity.setName(userDTO.getName().trim());
+        userEntity.setEmail(userDTO.getEmail().trim());
+        userEntity.setAge(userDTO.getAge());
+
+        // Saving to mongoDB
+        userEntity.persist();
         return Response.status(Response.Status.CREATED).entity("User added!").build();
+//        String id = UUID.randomUUID().toString();
+//        userDTO.setId(id);
+//        users.put(id, userDTO);
+//        saveUsersToFile();
     }
 
-    private void saveUsersToFile() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            objectMapper.writeValue(new File(String.valueOf(FILE_PATH)), users);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+//    private void saveUsersToFile() {
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        try {
+//            objectMapper.writeValue(new File(String.valueOf(FILE_PATH)), users);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
-    // Loading all users at start
-    private void loadUsersFromFile() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            File file = new File(String.valueOf(FILE_PATH));
-            if (file.exists()) {  // Check if the file exists
-                users = objectMapper.readValue(file, new TypeReference<Map<String, UserDTO>>() {});
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+//    // Loading all users at start
+//    private void loadUsersFromFile() {
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        try {
+//            File file = new File(String.valueOf(FILE_PATH));
+//            if (file.exists()) {  // Check if the file exists
+//                users = objectMapper.readValue(file, new TypeReference<Map<String, UserDTO>>() {});
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
     // Get user by ID
     public Response getSingleUser(String id) {
-        UserDTO user = users.get(id);
-        if(user == null) {
+        UserEntity userEntity = UserEntity.findById(id);
+//        UserDTO user = users.get(id);
+        if(userEntity == null) {
             return Response.status(Response.Status.NOT_FOUND).entity("User not found!").build();
         }
-        return Response.ok(user).build();
+        return Response.ok(userEntity).build();
     }
 
     // Update User by ID
     public Response updateUser(String id, UserDTO updatedUser) {
-        UserDTO user = users.get(id);
-        if (user == null) {
+        UserEntity userEntity = UserEntity.findById(id);
+        if (userEntity == null) {
             return Response.status(Response.Status.NOT_FOUND).entity("User not found!").build();
         }
 
         if (updatedUser.getName() == null || updatedUser.getName().trim().isEmpty()) {
+            userEntity.setName(updatedUser.getName().trim());
+        } else {
             return Response.status(Response.Status.BAD_REQUEST).entity("Name field cannot be empty").build();
+
         }
 
         if (updatedUser.getEmail() == null || updatedUser.getEmail().trim().isEmpty()) {
+            userEntity.setEmail(updatedUser.getEmail().trim());
+        } else {
             return Response.status(Response.Status.BAD_REQUEST).entity("Email field cannot be empty").build();
         }
 
         if (updatedUser.getAge() <= 0) {
+            userEntity.setAge(updatedUser.getAge());
+        } else {
             return Response.status(Response.Status.BAD_REQUEST).entity("Age must be a positive integer!").build();
         }
         try {
-            user.updateNewUser(updatedUser);
-            users.put(id, user);
-            saveUsersToFile();
+            userEntity.persist();
+//            user.updateNewUser(updatedUser);
+//            users.put(id, user);
+//            saveUsersToFile();
             return Response.ok("User has been updated").build();
         } catch (IllegalArgumentException e) {
             return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
@@ -131,12 +148,13 @@ public class UserService {
 
     // Delete User by ID
     public Response deleteUser(String id) {
-        UserDTO user = users.get(id);
-        if (user == null) {
+        UserEntity userEntity = UserEntity.findById(id);
+        if (userEntity == null) {
             return Response.status(Response.Status.NOT_FOUND).entity("User not found!").build();
         }
-        users.remove(id, user);
-        saveUsersToFile();
+        userEntity.delete();
+//        users.remove(id, user);
+//        saveUsersToFile();
         return Response.ok("User has been deleted!").build();
     }
 }
